2169baffc71ec65f6ea9ecfbb1893314838340a1
Nicola-9 <Nicola-9@users.noreply.github.com>
2020-10-12 12:40:56 +0200

    Created blog backend

--- api/controllers/articleController.js
+++ api/controllers/articleController.js
@@ -32,5 +32,75 @@ module.exports = {
                 data: article
             });
         });
+    },
+    getArticle: (req, res) =>{
+        let articleID = req.params.articleID;
+
+        Article.findById(articleID, (error, article) =>{
+            if(error){
+                return res.status(500).json({
+                    response: "UNSUCCESS", 
+                    error: "Internal Server Error" 
+                });
+            }
+
+            return res.status(200).json({
+                response: "SUCCESS",
+                data: article
+            });
+        });
+    },
+    patchArticle: (req, res) =>{
+        let fieldToUpdate = req.body;
+        let articleID = req.params.articleID;
+
+        Article.findOneAndUpdate({ _id: articleID }, { $set: fieldToUpdate }, { new: true }, (error, article) =>{
+            if(error){
+                return res.status(500).json({
+                    response: "UNSUCCESS", 
+                    error: "Internal Server Error" 
+                });
+            }
+
+            return res.status(200).json({
+                response: "SUCCESS",
+                data: article
+            });
+        });
+    },
+    putArticle: (req, res) =>{
+        let articleToUpdate = req.body;
+        let articleID = req.params.articleID;
+
+        Article.findOneAndUpdate({ _id: articleID }, articleToUpdate, (error, article) =>{
+            if(error){
+                return res.status(500).json({
+                    response: "UNSUCCESS", 
+                    error: "Internal Server Error" 
+                });
+            }
+
+            return res.status(200).json({
+                response: "SUCCESS",
+                data: article
+            });
+        });
+    },
+    deleteArticle: (req, res) =>{
+        let articleID = req.params.articleID;
+
+        Article.findByIdAndDelete(articleID, (error, article) =>{
+            if(error){
+                return res.status(500).json({
+                    response: "UNSUCCESS", 
+                    error: "Internal Server Error" 
+                });
+            }
+
+            return res.status(200).json({
+                response: "SUCCESS",
+                data: "Article successfully deleted"
+            });
+        });
     }
 }
--- api/routes/blogRoutes.js
+++ api/routes/blogRoutes.js
@@ -3,9 +3,15 @@ const articleController = require('../controllers/articleController');
 const routes = (app) =>{
     const articleController = require('../controllers/articleController');
 
-    app.route('/article')
+    app.route('/articles')
         .get(articleController.getAllArticles)
         .post(articleController.addNewArticle);
+
+    app.route('/articles/:articleID')
+        .get(articleController.getArticle)
+        .patch(articleController.patchArticle)
+        .put(articleController.putArticle)
+        .delete(articleController.deleteArticle);
 }
 
 module.exports = {
